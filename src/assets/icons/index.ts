e77a0dadea5f07fbd6fb267217bfc5094b5c49f2
JaeBin Yoon <dbswoqls89@snu.ac.kr>
2025-01-11 00:31:54 +0900

    update ico file export all together

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,15 @@
+import ComputerICO from "./computer_explorer.ico";
+import DocumentsICO from "@/assets/icons/directory_open_file_mydocs.ico";
+import InternetICO from "@/assets/icons/msie1.ico";
+import NetworkICO from "@/assets/icons/network_normal_two_pcs.ico";
+import RecycleBinICO from "@/assets/icons/recycle_bin_full.ico";
+import TopSecretICO from "@/assets/icons/users_key.ico";
+
+export {
+  ComputerICO,
+  DocumentsICO,
+  InternetICO,
+  NetworkICO,
+  RecycleBinICO,
+  TopSecretICO,
+};
