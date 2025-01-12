4ee9e1833498ec2c25a1798c1ec2f82dfbe95de8
JaeBin Yoon <dbswoqls89@snu.ac.kr>
2025-01-12 22:40:40 +0900

    change index page to use `useDesktop` custom hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,23 @@ import { Icon, StatusBar } from "@/components";
 import { msSans, msSansBold, libre } from "@/styles";
 import * as S from "./styled";
 import { useRef } from "react";
-import { css } from "@emotion/react";
-import { useBlockArea, useIntersection } from "@/hooks";
 import { useDesktop } from "@/hooks";
 import { icons } from "@/constants";
 
 export default function Home() {
-  const { width, height, top, left, onMouseDown, onMouseMove, onMouseUp } =
-    useBlockArea(anchorRef);
-
   const desktopRef = useRef<HTMLDivElement | null>(null);
   const blockAreaRef = useRef<HTMLDivElement | null>(null);
 
   const iconsRef = useRef<HTMLDivElement[]>([]);
-  useIntersection(blockAreaRef, iconsRef);
 
-  // 여기서 아이콘 배열들을 한꺼번에 관리해야 함.
+  const {
+    blockAreaStyle,
+    desktopEventHandler,
+    tempSelectedIcons,
+    selectedIcons,
+    prevClickedIconIdx,
+    clickedIconIdx,
+  } = useDesktop({ desktopRef, iconsRef, blockAreaRef });
 
   return (
     <>
@@ -34,9 +35,6 @@ export default function Home() {
       <S.Desktop
         ref={desktopRef}
         className={`${msSans.variable} ${msSansBold.variable} ${libre.variable}`}
-        onMouseDown={onMouseDown}
-        onMouseMove={onMouseMove}
-        onMouseUp={onMouseUp}
         {...desktopEventHandler}
       >
         <S.BlockArea ref={blockAreaRef} style={{ ...blockAreaStyle }} />
