01b55b998d8a47cbe431f6d2775251fd54633d6f
JaeBin Yoon <dbswoqls89@snu.ac.kr>
2025-01-14 02:22:17 +0900

    add onClick handler opening modal to `Icon` component

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,8 +1,27 @@
+import { useDoubleClick } from "@/hooks";
 import * as S from "./styled";
+import { useModal } from "@/providers";
+import { modals } from "@/components/modals";
 
 export const Icon = ({ name, ico, ref, highlighted, outlined }) => {
+  const { open, close } = useModal();
+  const { onDoubleClick } = useDoubleClick([
+    () =>
+      open(
+        modals[name],
+        { handleClose: () => close(name) },
+        { name, ico: ico }
+      ), 
+  ]);
+
   return (
-    <S.IconContainer ref={ref} onMouseDown={(e) => e.stopPropagation()}>
+    <S.IconContainer
+      ref={ref}
+      onClick={(e) => {
+        e.stopPropagation();
+        onDoubleClick();
+      }}
+    >
       <S.IconImg src={ico.src} />
       <S.IconName highlighted={highlighted} outlined={outlined}>
         {name}
