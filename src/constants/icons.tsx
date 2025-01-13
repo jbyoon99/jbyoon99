99aa333c77bef2dd65bf677f0dd3e5dac35bdf9f
JaeBin Yoon <dbswoqls89@snu.ac.kr>
2025-01-13 21:09:32 +0900

    delete unused icons

--- src/constants/icons.tsx
+++ src/constants/icons.tsx
@@ -1,44 +1,7 @@
-import {
-  ComputerICO,
-  DocumentsICO,
-  InternetICO,
-  NetworkICO,
-  RecycleBinICO,
-  TopSecretICO,
-} from "@/assets/icons";
+import { DocumentsICO, RecycleBinICO, TopSecretICO } from "@/assets/icons";
 
 export const icons = [
-  { ico: ComputerICO, name: "My Computer" },
   { ico: DocumentsICO, name: "My Documents" },
-  {
-    ico: InternetICO,
-    name: (
-      <>
-        Internet
-        <br />
-        Explorer
-      </>
-    ),
-  },
-  {
-    ico: NetworkICO,
-    name: (
-      <>
-        Network
-        <br />
-        Neighborhood
-      </>
-    ),
-  },
   { ico: RecycleBinICO, name: "Recycle Bin" },
-  {
-    ico: TopSecretICO,
-    name: (
-      <>
-        JByoon99
-        <br />
-        Top Secret
-      </>
-    ),
-  },
+  { ico: TopSecretICO, name: "Copyrights" },
 ];
