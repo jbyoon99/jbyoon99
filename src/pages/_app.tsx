c3afb2ab8f07824835c134c8b716f30203bf21f1
JaeBin Yoon <dbswoqls89@snu.ac.kr>
2025-01-13 16:28:00 +0900

    add `ModalProvider` in `_app.tsx`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import { ModalProvider } from "@/providers";
 import "@/styles/global.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ModalProvider>
+      <Component {...pageProps} />
+    </ModalProvider>
+  );
 }
