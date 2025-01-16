cc49e69c4cbe11d6ec7c86d3a6d35a267c5a6f43
JaeBin Yoon <dbswoqls89@snu.ac.kr>
2025-01-16 15:25:29 +0900

    delete constants directory

--- src/constants/icons.tsx
+++
@@ -1,7 +0,0 @@
-import { DocumentsICO, RecycleBinICO, TopSecretICO } from "@/assets/icons";
-
-export const icons = [
-  { ico: DocumentsICO, name: "My Documents" },
-  { ico: RecycleBinICO, name: "Recycle Bin" },
-  { ico: TopSecretICO, name: "Copyrights" },
-];
