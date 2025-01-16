635c43b3ec4942bb8fa83b83b7410e9b2055f332
JaeBin Yoon <dbswoqls89@snu.ac.kr>
2025-01-17 01:21:44 +0900

    refactor `useBlockArea` custom hook

--- src/hooks/desktop/useBlockArea.ts
+++ src/hooks/desktop/useBlockArea.ts
@@ -1,12 +1,12 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
 export const useBlockArea = ({
   desktopRef,
-  blockAreaRef,
   iconsRef,
   initialPoint,
   currentPoint,
   setTempSelectedIcons,
+  isDrawingBlockArea,
 }) => {
   const [desktopPosition, setDesktopPosition] = useState<null | {
     x: number;
@@ -18,45 +18,22 @@ export const useBlockArea = ({
     top: 0,
     left: 0,
   });
+  const POINTER_OFFSET = useMemo(() => 6, []);
 
   // TODO : 모달이 떠있을 때 끊기지 않도록 하기.
   // 모달 자체에서 블록이 잡히지 않도록 하면서 밖에서 했을 때는 그대로 이어지도록
 
-  useEffect(() => {
-    if (!blockAreaRef || !iconsRef) return;
-    const { width, height, top, left } =
-      blockAreaRef.current.getBoundingClientRect();
-    const _tempSelectedIcons = iconsRef.current.map((icon: HTMLDivElement) => {
-      const {
-        width: iw,
-        height: ih,
-        top: it,
-        left: il,
-      } = icon.getBoundingClientRect();
-      const isXIntersect = il <= left + width && il + iw >= left;
-      const isYIntersect = it <= top + height && ih + it >= top;
-      return isXIntersect && isYIntersect;
-    });
-    setTempSelectedIcons(_tempSelectedIcons);
-  }, [blockAreaPosition, blockAreaRef, iconsRef, setTempSelectedIcons]);
+  const getSelectionRect = useCallback((start, end, wrapperPosition) => {
+    const { x: sx, y: sy } = start;
+    const { x: ex, y: ey } = end;
+    const { x: wx, y: wy } = wrapperPosition;
+    const width = Math.abs(sx - ex);
+    const height = Math.abs(sy - ey);
+    const left = Math.abs(wx - Math.min(sx, ex) + POINTER_OFFSET);
+    const top = Math.abs(wy - Math.min(sy, ey) + POINTER_OFFSET);
 
-  useEffect(() => {
-    if (!desktopPosition || !initialPoint || !currentPoint) {
-      setBlockAreaPosition({ width: 0, height: 0, left: 0, top: 0 });
-      return;
-    }
-
-    const width = Math.abs(initialPoint.x - currentPoint.x);
-    const height = Math.abs(initialPoint.y - currentPoint.y);
-    const left = Math.abs(
-      desktopPosition.x - Math.min(initialPoint.x, currentPoint.x) + 6
-    );
-    const top = Math.abs(
-      desktopPosition.y - Math.min(initialPoint.y, currentPoint.y) + 6
-    );
-
-    setBlockAreaPosition({ width, height, left, top });
-  }, [desktopPosition, initialPoint, currentPoint]);
+    return { width, height, left, top };
+  }, []);
 
   useEffect(() => {
     if (!desktopRef) return;
@@ -64,6 +41,7 @@ export const useBlockArea = ({
     const updateDesktopPosition = () => {
       const { x, y } = desktopRef.current.getBoundingClientRect();
       setDesktopPosition({ x, y });
+      console.log(x, y);
     };
 
     window.addEventListener("resize", updateDesktopPosition);
@@ -73,6 +51,41 @@ export const useBlockArea = ({
       window.removeEventListener("resize", updateDesktopPosition);
     };
   }, [desktopRef]);
+
+  useEffect(() => {
+    if (!iconsRef || !desktopPosition || !initialPoint || !currentPoint) return;
+
+    const { width, height, left, top } = getSelectionRect(
+      initialPoint,
+      currentPoint,
+      desktopPosition
+    );
+    const _tempSelectedIcons = iconsRef.current.map((icon: HTMLDivElement) => {
+      const {
+        width: iw,
+        height: ih,
+        top: it,
+        left: il,
+      } = icon.getBoundingClientRect();
+      const isXIntersect =
+        il <= left + width + desktopPosition.x + POINTER_OFFSET &&
+        il + iw >= left + desktopPosition.x + POINTER_OFFSET;
+      const isYIntersect =
+        it <= top + height + desktopPosition.y + POINTER_OFFSET &&
+        ih + it >= top + desktopPosition.y + POINTER_OFFSET;
+      return isXIntersect && isYIntersect;
+    });
+    setTempSelectedIcons(_tempSelectedIcons);
+    setBlockAreaPosition({ width, height, top, left });
+  }, [
+    iconsRef,
+    setTempSelectedIcons,
+    desktopPosition,
+    initialPoint,
+    currentPoint,
+    getSelectionRect,
+    POINTER_OFFSET,
+  ]);
 
   return { blockAreaPosition };
 };
