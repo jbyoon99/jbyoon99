78193827e5bf90410fee90ddad9b4206f70dae34
JaeBin Yoon <dbswoqls89@snu.ac.kr>
2025-01-17 05:09:33 +0900

    refactor `useBlockArea` custom hook

--- src/hooks/desktop/useBlockArea.ts
+++ src/hooks/desktop/useBlockArea.ts
@@ -1,36 +1,66 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useReducer, useRef } from "react";
+
+const blockAreaReducer = (state, action) => {
+  switch (action.type) {
+    case "reset":
+      return { width: 0, height: 0, top: 0, left: 0 };
+
+    case "update":
+      return action.value;
+
+    default:
+      return state;
+  }
+};
 
 export const useBlockArea = ({
   desktopRef,
   iconsRef,
-  initialPoint,
-  currentPoint,
-  setTempSelectedIcons,
-  isDrawingBlockArea,
+  setSelectedIcons,
+  cursorPoint,
 }) => {
-  const [desktopPosition, setDesktopPosition] = useState<null | {
-    x: number;
-    y: number;
-  }>(null);
-  const [blockAreaPosition, setBlockAreaPosition] = useState({
+  const desktopBoundingRect = useRef(null);
+  const [state, dispatch] = useReducer(blockAreaReducer, {
     width: 0,
     height: 0,
     top: 0,
     left: 0,
   });
-  const POINTER_OFFSET = useMemo(() => 6, []);
 
-  // TODO : 모달이 떠있을 때 끊기지 않도록 하기.
-  // 모달 자체에서 블록이 잡히지 않도록 하면서 밖에서 했을 때는 그대로 이어지도록
-
-  const getSelectionRect = useCallback((start, end, wrapperPosition) => {
+  const getSelectionRect = useCallback((start, end) => {
+    const POINTER_OFFSET = { x: 5, y: 6 };
+    const STATUS_BAR_HEIGHT = 30;
     const { x: sx, y: sy } = start;
     const { x: ex, y: ey } = end;
-    const { x: wx, y: wy } = wrapperPosition;
-    const width = Math.abs(sx - ex);
-    const height = Math.abs(sy - ey);
-    const left = Math.abs(wx - Math.min(sx, ex) + POINTER_OFFSET);
-    const top = Math.abs(wy - Math.min(sy, ey) + POINTER_OFFSET);
+    const { x: dx, y: dy, width: dw, height: dh } = desktopBoundingRect.current;
+
+    const isOutOfLeft = ex - POINTER_OFFSET.x <= dx;
+    const isOutOfRight = ex + POINTER_OFFSET.x > dx + dw;
+    const isOutOfTop = ey - POINTER_OFFSET.y <= dy;
+    const isOutOfBottom = ey + POINTER_OFFSET.y > dy + dh - STATUS_BAR_HEIGHT;
+    let width, height, left, top;
+
+    width = Math.abs(sx - ex);
+    height = Math.abs(sy - ey);
+    left = Math.abs(dx - Math.min(sx, ex) + POINTER_OFFSET.x);
+    top = Math.abs(dy - Math.min(sy, ey) + POINTER_OFFSET.y);
+
+    if (isOutOfLeft) {
+      left = 0;
+      width = Math.abs(sx - dx) - POINTER_OFFSET.x;
+    }
+    if (isOutOfTop) {
+      top = 0;
+      height = Math.abs(sy - dy) - POINTER_OFFSET.y;
+    }
+    if (isOutOfRight) {
+      left = sx - dx - POINTER_OFFSET.x;
+      width = dx + dw - sx - POINTER_OFFSET.x;
+    }
+    if (isOutOfBottom) {
+      top = sy - dy - POINTER_OFFSET.y;
+      height = dy + dh - STATUS_BAR_HEIGHT - sy;
+    }
 
     return { width, height, left, top };
   }, []);
@@ -39,9 +69,7 @@ export const useBlockArea = ({
     if (!desktopRef) return;
 
     const updateDesktopPosition = () => {
-      const { x, y } = desktopRef.current.getBoundingClientRect();
-      setDesktopPosition({ x, y });
-      console.log(x, y);
+      desktopBoundingRect.current = desktopRef.current.getBoundingClientRect();
     };
 
     window.addEventListener("resize", updateDesktopPosition);
@@ -53,39 +81,34 @@ export const useBlockArea = ({
   }, [desktopRef]);
 
   useEffect(() => {
-    if (!iconsRef || !desktopPosition || !initialPoint || !currentPoint) return;
+    if (!iconsRef || !cursorPoint.initial) {
+      dispatch({ type: "reset" });
+      return;
+    }
 
-    const { width, height, left, top } = getSelectionRect(
-      initialPoint,
-      currentPoint,
-      desktopPosition
-    );
-    const _tempSelectedIcons = iconsRef.current.map((icon: HTMLDivElement) => {
+    const { initial, current } = cursorPoint;
+    const { width, height, left, top } = getSelectionRect(initial, current);
+    const POINTER_OFFSET = { x: 5, y: 6 };
+    dispatch({ type: "update", value: { width, height, top, left } });
+
+    const _selectedIcons = iconsRef.current.map((icon: HTMLDivElement) => {
       const {
         width: iw,
         height: ih,
         top: it,
         left: il,
       } = icon.getBoundingClientRect();
+      const { x: dx, y: dy } = desktopBoundingRect.current;
       const isXIntersect =
-        il <= left + width + desktopPosition.x + POINTER_OFFSET &&
-        il + iw >= left + desktopPosition.x + POINTER_OFFSET;
+        il <= left + width + dx + POINTER_OFFSET.x &&
+        il + iw >= left + dx + POINTER_OFFSET.x;
       const isYIntersect =
-        it <= top + height + desktopPosition.y + POINTER_OFFSET &&
-        ih + it >= top + desktopPosition.y + POINTER_OFFSET;
+        it <= top + height + dy + POINTER_OFFSET.y &&
+        ih + it >= top + dy + POINTER_OFFSET.y;
       return isXIntersect && isYIntersect;
     });
-    setTempSelectedIcons(_tempSelectedIcons);
-    setBlockAreaPosition({ width, height, top, left });
-  }, [
-    iconsRef,
-    setTempSelectedIcons,
-    desktopPosition,
-    initialPoint,
-    currentPoint,
-    getSelectionRect,
-    POINTER_OFFSET,
-  ]);
+    setSelectedIcons(_selectedIcons);
+  }, [iconsRef, setSelectedIcons, cursorPoint, getSelectionRect]);
 
-  return { blockAreaPosition };
+  return { blockAreaStyle: state };
 };
