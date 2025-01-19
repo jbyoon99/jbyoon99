c0a23111f17b69168f9191bf2674ddde2b6ef440
JaeBin Yoon <dbswoqls89@snu.ac.kr>
2025-01-19 23:27:18 +0900

    change directory structure of ico assets

---
+++ src/assets/ico/index.ts
@@ -0,0 +1,15 @@
+import ComputerICO from "./computer_explorer.ico";
+import DocumentsICO from "./directory_open_file_mydocs.ico";
+import InternetICO from "./msie1.ico";
+import NetworkICO from "./network_normal_two_pcs.ico";
+import RecycleBinICO from "./recycle_bin_full.ico";
+import TopSecretICO from "./users_key.ico";
+
+export {
+  ComputerICO,
+  DocumentsICO,
+  InternetICO,
+  NetworkICO,
+  RecycleBinICO,
+  TopSecretICO,
+};
