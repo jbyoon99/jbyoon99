8f8bc3fc7ed201a5ccc3db4ce97d676bde02eae8
JaeBin Yoon <dbswoqls89@snu.ac.kr>
2025-01-20 01:30:40 +0900

    change img asset names

--- src/assets/ico/index.ts
+++ src/assets/ico/index.ts
@@ -1,15 +1,17 @@
-import ComputerICO from "./computer_explorer.ico";
-import DocumentsICO from "./directory_open_file_mydocs.ico";
-import InternetICO from "./msie1.ico";
-import NetworkICO from "./network_normal_two_pcs.ico";
-import RecycleBinICO from "./recycle_bin_full.ico";
-import TopSecretICO from "./users_key.ico";
+import ComputerICO from "./computer.ico";
+import DirectoryICO from "./directory.ico";
+import InternetICO from "./internet.ico";
+import NetworkICO from "./network.ico";
+import BinICO from "./bin.ico";
+import UsersKeyIco from "./users_key.ico";
+import NotepadICO from "./notepad.ico";
 
 export {
   ComputerICO,
-  DocumentsICO,
+  DirectoryICO,
   InternetICO,
   NetworkICO,
-  RecycleBinICO,
-  TopSecretICO,
+  BinICO,
+  UsersKeyIco,
+  NotepadICO,
 };
--- src/assets/png/index.ts
+++ src/assets/png/index.ts
@@ -1,5 +1,5 @@
 import GithubPNG from "@/assets/png/github.png";
 import LinkedInPNG from "@/assets/png/linkedin.png";
-import StartIconPNG from "@/assets/png/jbyoon99_start_icon.png";
+import StartIconPNG from "@/assets/png/jbyoon99_small_logo.png";
 
 export { GithubPNG, LinkedInPNG, StartIconPNG };
