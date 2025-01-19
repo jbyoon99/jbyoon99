b20532b983dd46684355911bc232dfa73e2f39c6
JaeBin Yoon <dbswoqls89@snu.ac.kr>
2025-01-19 23:27:36 +0900

    change directory structure of png assets

---
+++ src/assets/png/index.ts
@@ -0,0 +1,5 @@
+import GithubPNG from "@/assets/png/github.png";
+import LinkedInPNG from "@/assets/png/linkedin.png";
+import StartIconPNG from "@/assets/png/jbyoon99_start_icon.png";
+
+export { GithubPNG, LinkedInPNG, StartIconPNG };
