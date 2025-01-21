e8d8ed57bc86e002323be5e85b01c34d43bc9d20
JaeBin Yoon <dbswoqls89@snu.ac.kr>
2025-01-21 17:21:49 +0900

    add `Notepad` modal component

--- src/components/modals/Notepad/index.tsx
+++ src/components/modals/Notepad/index.tsx
@@ -1,3 +1,26 @@
-export const NotepadModal = () => {
-  return <></>;
+import * as S from "./styled";
+import { ModalLayout } from "../Layout";
+
+export const NotepadModal = (props) => {
+  return (
+    <ModalLayout variant="notepad" {...props}>
+      <S.Toolbar>
+        <span>
+          <u>F</u>ile
+        </span>
+        <span>
+          <u>E</u>dit
+        </span>
+        <span>
+          <u>S</u>earch
+        </span>
+        <span>
+          <u>H</u>elp
+        </span>
+      </S.Toolbar>
+      <S.Container>
+        <S.Content></S.Content>
+      </S.Container>
+    </ModalLayout>
+  );
 };
