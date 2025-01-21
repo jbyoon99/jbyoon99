d3e0a9a522fb8bbefe1abda06752efa54acb4065
JaeBin Yoon <dbswoqls89@snu.ac.kr>
2025-01-21 17:21:32 +0900

    add `Directory` modal component

--- src/components/modals/Directory/index.tsx
+++ src/components/modals/Directory/index.tsx
@@ -1,3 +1,40 @@
-export const DirectoryModal = () => {
-  return <></>;
+import * as S from "./styled";
+import { ModalLayout } from "../Layout";
+import { JByoonLogoICO } from "@/assets";
+
+const options = [
+  <S.Option key={1}>
+    <u>F</u>ile
+  </S.Option>,
+  <S.Option key={2}>
+    <u>E</u>dit
+  </S.Option>,
+  <S.Option key={3}>
+    <u>V</u>iew
+  </S.Option>,
+  <S.Option key={4}>
+    <u>G</u>o
+  </S.Option>,
+  <S.Option key={5}>
+    F<u>a</u>vorites
+  </S.Option>,
+  <S.Option key={6}>
+    <u>H</u>elp
+  </S.Option>,
+];
+
+export const DirectoryModal = (props) => {
+  return (
+    <ModalLayout variant="notepad" {...props}>
+      <S.Toolbar>
+        <S.OptionContainer>{options.map((option) => option)}</S.OptionContainer>
+        <S.LogoContainer>
+          <S.Logo src={JByoonLogoICO.src} />
+        </S.LogoContainer>
+      </S.Toolbar>
+      <S.Container>
+        <S.Content></S.Content>
+      </S.Container>
+    </ModalLayout>
+  );
 };
---
+++ src/components/modals/Directory/styled.ts
@@ -0,0 +1,81 @@
+import styled from "@emotion/styled";
+
+export const Toolbar = styled.div`
+  font-family: var(--font-ms-sans);
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  height: 3rem;
+  border-top: 1px solid #cfd6ca;
+  border-bottom: 1px solid #77787e;
+  box-shadow: 0 1px 0 #000;
+  z-index: 1;
+  
+
+  span {
+    margin-right: 1rem;
+    font-size: 1.1rem;
+  }
+`;
+
+export const OptionContainer = styled.div``
+
+export const Option = styled.span``
+
+export const Content = styled.div`
+  background-color: white;
+  flex-grow: 1;
+  padding: 1rem;
+  font-size: 2rem;
+  font-family: var(--font-ms-sans);
+  user-select: text;
+`;
+
+export const ScrollBar = styled.div<{ isHorizontal: boolean }>`
+  display: flex;
+  flex-direction: ${(props) => (props.isHorizontal ? "row" : "column")};
+  align-items: center;
+`;
+
+export const ThumbContainer = styled.div`
+  width: 1.5rem;
+  height: 1.5rem;
+  border: 0.1rem solid;
+  border-color: #fff #808080 #808080 #fff;
+  box-shadow: 0.1rem 0 0 #000000, -0.1rem 0 0 #dfdfdf, 0 0.1rem 0 #000000,
+    0 -0.1rem 0 #dfdfdf;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Thumb = styled.img`
+  width: 0.7rem;
+  height: 0.5rem;
+`;
+
+export const Bar = styled.div`
+  height: 100%;
+  width: 100%;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-grow: 1;
+`;
+
+export const LogoContainer = styled.div`
+  height: 100%;
+  width: 5rem;
+  background-color: black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: radial-gradient(circle, white 0%, black 40%);
+`;
+
+export const Logo = styled.img`
+  width: 2rem;
+`;
