740edbee69f6806f12ccac23a71997977ee28f62
JaeBin Yoon <dbswoqls89@snu.ac.kr>
2025-01-22 23:54:41 +0900

    add recyclebinTempalte

---
+++ src/templates/recycleBin/index.ts
@@ -0,0 +1,11 @@
+import { NotepadICO } from "@/assets";
+import { NotepadModal } from "@/components";
+
+export const recycleBinTemplate = [
+  {
+    name: "My old dream",
+    Component: NotepadModal,
+    ico: NotepadICO,
+    content: "My dream was to be movie director",
+  },
+];
