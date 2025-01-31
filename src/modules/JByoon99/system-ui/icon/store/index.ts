e03899d65b7b916622b1eaf793de9897906f9db5
JaeBin Yoon <dbswoqls89@snu.ac.kr>
2025-01-31 23:25:22 +0900

    add `icon` module store

---
+++ src/modules/JByoon99/system-ui/icon/store/index.ts
@@ -0,0 +1,9 @@
+import { BinICO, DirectoryICO, UsersKeyIco } from "assets";
+
+export const iconList = {
+  desktop: [
+    { name: "My Documents", icon: DirectoryICO.src },
+    { name: "Recycle Bin", icon: BinICO.src },
+    { name: "Copyrights", icon: UsersKeyIco.src },
+  ],
+};
